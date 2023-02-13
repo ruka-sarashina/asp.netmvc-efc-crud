97c5fbcadbd94401771197f4bf205ce7674eff1e
taufik-fauzi <novops911@gmail.com>
2023-02-13 15:24:50 +0700

    first commit

---
+++ ASPMVCCRUD/ASPMVCCRUD/Data/MVCDEMODbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPMVCCRUD.Data
+{
+    public class MVCDEMODbContext : DbContext //MVCDEMODbContext inheritance dari DbContext
+    {
+       // Opsi Constructor ini akn mengembalikkan kembali ke kelas awal
+        public MVCDEMODbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // Sebagai entitas model untuk data karyawan dan melakukan crud 
+        public DbSet<> MyProperty { get; set; }
+        
+    }
+}
---
+++ ASPMVCCRUD/ASPMVCCRUD/Models/Domain/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPMVCCRUD.Models.Domain
+{
+    // sekrang mari buat beberapa properti untuk karyawan
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
+ 
